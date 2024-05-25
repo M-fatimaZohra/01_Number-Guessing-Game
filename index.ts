782e7b0cff08060c207e1067be750f570ecc48c3
M.A COM <fatima.zohra101220007@gmail.com>
2024-05-25 02:53:49 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,35 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+
+const botGuess: number = Math.random() * 10;
+
+const roundOf: number = Math.ceil(botGuess);
+
+console.log(`🎶Guess number from 0 to 10 and test your luck🎶`);
+
+const result = await inquirer.prompt([
+  {
+    name: "GuessNumber",
+    type: "number",
+    message: "Test you luck by puting a random number ✍ ",
+  },
+]);
+
+console.log(`\n`);
+
+console.log(`YOUR GUESS NUMBER IS ${result.GuessNumber}`);
+
+console.log(`\n`);
+
+if (roundOf === result.GuessNumber) {
+  console.log(`-------------------------------`);
+  console.log(`|${roundOf} = ${result.GuessNumber}🎉 you are a lucky 🎉| \n`);
+  console.log(`-------------------------------`);
+}
+
+else {
+  console.log(`-------------------------------`);
+  console.log(`|${roundOf} ≠ ${result.GuessNumber}  😢 BETTER LUCK NEXT TIME|`);
+  console.log(`-------------------------------`);
+}
+ 
